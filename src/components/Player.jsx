125f52832e368b3e54d7aa973ea0e06cf1810a30
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-04 16:23:10 +0330

    manage submitted state & add event listener & handler to 'Set Name' button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 export default function Player() {
+  const [submitted, setSubmitted] = useState(false);
   const [playerName, setPlayerName] = useState("");
   function handleChange(event) {
     setPlayerName(event.target.value);
+  }
+  function handleClick() {
+    setSubmitted(true);
   }
   return (
     <section id="player">
       <h2>Welcome unknown entity</h2>
       <p>
         <input type="text" onChange={handleChange} value={playerName} />
-        <button>Set Name</button>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
