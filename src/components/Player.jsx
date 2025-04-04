17e687a10deaafc57c6b2a70f32495ef88c301f6
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-04 16:16:42 +0330

    manage playerName state & add event listener & handler to input element

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
   return (
     <section id="player">
       <h2>Welcome unknown entity</h2>
       <p>
-        <input type="text" />
+        <input type="text" onChange={handleChange} value={playerName} />
         <button>Set Name</button>
       </p>
     </section>
