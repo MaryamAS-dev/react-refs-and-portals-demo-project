d206fd37936f959e08ffc571b131069d88510d7a
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-05 00:40:52 +0330

    manage timerExpired state & add event listener & handler to 'Start Challenge' button to start the timer

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  // start a timer that expires after the targetTime in seconds, then set timerExpired to true
+  function handleStart() {
+    // set a timer using setTimeout function
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
@@ -6,7 +15,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>Start Challenge</button>
       </p>
       <p className=""> Time is running... / Timer inactive</p>
     </section>
