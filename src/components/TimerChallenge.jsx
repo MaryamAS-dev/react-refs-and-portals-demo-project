85c42e8844c2b9a51598b4ff500149e1b6999b86
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-05 15:03:40 +0330

    use useRef to store timer ID, preventing reset on component re-render

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 export default function TimerChallenge({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
+  // create a reference to store the timer ID, which persists across re-renders
+  // and doesn't reset when the component re-renders
+  const timerRef = useRef();
   // start a timer that expires after the targetTime in seconds, then set timerExpired to true
   function handleStart() {
-    setTimerStarted(true);
     // set a timer using setTimeout function
-    setTimeout(() => {
+    timerRef.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+  // stop the timer
+  function handleStop() {
+    clearTimeout(timerRef.current);
   }
   return (
     <section className="challenge">
@@ -19,7 +27,8 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        {/* execute handleStop if timerStarted is true, otherwise execute handleStart */}
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
