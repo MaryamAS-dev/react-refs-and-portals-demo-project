d9efbca0fb6d8aae8432c711db428ee9bb941fea
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-08 23:37:11 +0330

    use createPortal to render the modal outside the main DOM hierarchy

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 export default function ResultModal({
   ref,
   targetTime,
@@ -19,7 +20,8 @@ export default function ResultModal({
   });
   // create a ref to access the dialog element directly, ensuring it's only used within the ResultModal component
   const dialogRef = useRef();
-  return (
+  // render the modal outside the main DOM hierarchy using createPortal
+  return createPortal(
     // reset the timeRemaining state when the dialog is closed via the escape key
     <dialog ref={dialogRef} className="result-modal" onClose={onReset}>
       {/* conditionally display a "You lost!" message if the user has lost */}
@@ -38,6 +40,7 @@ export default function ResultModal({
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal") // render modal into a specific DOM node (modal-root)
   );
 }
